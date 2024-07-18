c58fef8503e735fe1d049883a2d3b5033ee28f07
Angelica Tacca Dughetti <76002148+angietd94@users.noreply.github.com>
2024-07-18 12:08:59 +0200

    Update launchfirst.sh

---
+++ launchfirst.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/angietd94/confluent-self-managed-onpremise.git
+cd confluent-self-managed-onpremise
+
+
+chmod +x executable.sh 
+chmod +x config_files.sh
+chmod +x config.sh
+chmod +x ccloud-generate-cp-configs.sh
+./executable.sh
